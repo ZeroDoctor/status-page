32daf940e5a5601849db7fe9fcc6f1a492806779
zerodoctor <danielrcastro10@gmail.com>
2021-01-28 22:42:23 -0600

    load dot env

--- main.go
+++ main.go
@@ -8,9 +8,56 @@ import (
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/zerodoctor/go-status/handler"
 	ppt "github.com/zerodoctor/goprettyprinter"
 )
+
+var env = ""
+
+func init() {
+	ppt.Init()
+	ppt.LoggerFlags = ppt.FILE | ppt.LINE
+	ppt.Infoln("Starting Server...")
+
+	env := os.Getenv("env")
+	envFile := "./" + env + ".env"
+	if env == "" {
+		env = "dev"
+		envFile = env + ".env"
+	}
+
+	err := godotenv.Load(envFile)
+	if err != nil {
+		ppt.Errorln(err.Error())
+		panic("Failed to load env file")
+	}
+}
+
+func main() {
+	dbh := handler.NewDBHandler()
+	wh := handler.NewWebHandler(dbh)
+
+	router := gin.Default()
+
+	html := template.Must(loadTemplates("./assets/html"))
+	router.SetHTMLTemplate(html)
+
+	router.Static("/css", "./assets/css/")
+	router.Static("/js", "./assets/js/")
+	router.Static("/img", "./assets/img/")
+	router.StaticFile("/favicon.ico", "./assets/favicon.ico")
+
+	router.GET("/", wh.RenderIndex)
+	router.GET("/ws", wh.Websocket)
+	router.POST("/new/app", wh.NewApp)
+	// TODO: create init route
+
+	go wh.WsBroadcast()
+	go SendFake(wh)
+
+	router.Run(":3000")
+}
 
 func loadTemplates(path string) (*template.Template, error) {
 
@@ -36,32 +83,4 @@ func loadTemplates(path string) (*template.Template, error) {
 	}
 
 	return template.ParseFiles(files...)
-}
-
-func main() {
-	ppt.Init()
-	ppt.Infoln("Starting Server...")
-
-	dbh := handler.NewDBHandler()
-	wh := handler.NewWebHandler(dbh)
-
-	router := gin.Default()
-
-	html := template.Must(loadTemplates("./assets/html"))
-	router.SetHTMLTemplate(html)
-
-	router.Static("/css", "./assets/css/")
-	router.Static("/js", "./assets/js/")
-	router.Static("/img", "./assets/img/")
-	router.StaticFile("/favicon.ico", "./assets/favicon.ico")
-
-	router.GET("/", wh.RenderIndex)
-	router.GET("/ws", wh.Websocket)
-	router.POST("/new/app", wh.NewApp)
-	// TODO: create init route
-
-	go wh.WsBroadcast()
-	go SendFake(wh)
-
-	router.Run(":3000")
 }
